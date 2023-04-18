c2a1b4bd778a43472b9937a11c396f6fd3219404
fullstackdassi1995 <112422387+fullstackdassi1995@users.noreply.github.com>
2023-04-18 21:45:17 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,30 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+
+public class Main {
+    public static AtomicInteger meet = new AtomicInteger(0);
+    protected static AtomicBoolean gameOver = new AtomicBoolean(false);
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] city = {"Israel", "United Kingdom", "USA", "Egypt" ,"Germany", "France",
+                "Poland", "Cyprus", "India" ,"Japan", "China", "Morocco", "Hungary","Switzerland",
+                "Russia", "Ukraine", "Greece", "Italy", "Argentina", "Sri Lanka" };
+
+        for (int i = 0; i < city.length; i++) {
+            new OlympicRunner(city[i]).start();
+
+        }
     }
+
+
 }
--- src/main/java/org/example/OlympicRunner.java
+++ src/main/java/org/example/OlympicRunner.java
@@ -1,2 +1,66 @@
-package org.example;public class OlympicRunner {
+package org.example;
+
+import java.util.Random;
+
+import static org.example.Main.*;
+
+
+public class OlympicRunner extends Thread{
+    protected String city;
+    protected Integer meter = 0;
+    Random random = new Random();
+
+
+    public OlympicRunner(String city) {
+        this.city = city;
+    }
+    public void run(){
+        while (!gameOver.get()){
+            for (int i = 0; i < random.nextInt(20000)+500; i++) {
+                if (meter == 45000){
+                    gameOver.set(true);
+
+                    try {
+                        sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    System.out.println("the winner is " + this.city + " " + this.meter);
+                    gameOver();
+                    break;
+                }
+
+                else meter++;
+
+            }
+
+            if (gameOver.get()){
+                break;
+            }
+
+
+            try {
+                sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(this.city + " " + this.meter);
+            boom();
+            meet.set(meter);
+        }
+
+    }
+
+public void boom(){
+        if (meet.get() == meter & meter > 100){
+            meter = 0;
+        }
+}
+
+public void gameOver(){
+    if (gameOver.get()){
+        OlympicRunner.currentThread().stop();
+    }
+}
 }
